2a0f9012ada78f6a25f3d79c87270b2066c6c3c5
PitchBlackNights <pitchblacknights@proton.me>
2023-05-17 22:11:09 -0500

    Formatting and added Master bit fault detection

--- src/data_checker.rs
+++ src/data_checker.rs
@@ -1,22 +1,46 @@
-pub fn hamming(bits: [i32; 16]) -> i32 {
-  let mut prev: i32 = 0;
-  let mut stack: Vec<i32> = Vec::new();
+pub fn hamming(bits: &[i32; 16]) -> [i32; 2] {
+    //! FUNCTION BREAKS IF THERE IS MORE THAN 2 ERRORED BITS
+    //* RETURNS:
+    //* [Location of errored bit (IS ONE OF 0..16), If master bit is wrong (IS EITHER 0 or 1)]
 
-  // Remove all zero bits and pushes to stack
-  for i in 0..16 {
-      if bits[i] == 1 {
-          stack.push(i as i32);
-      }
-  }
+    // Remove all zero bits and pushes to stack
+    let mut master_stack: Vec<i32> = Vec::new();
+    for i in 0..16 {
+        if bits[i] == 1 {
+            master_stack.push(i as i32);
+        }
+    }
 
-  // Pushes xor(prev, next) to prev
-  for i in 0..stack.len() {
-      if i as i32 != 0 {
-          prev = prev ^ stack[i];
-      } else {
-          prev = stack[0];
-      }
-  }
+    // Pushes xor(prev, next) to prev
+    let mut prev: i32 = master_stack[0];
+    for i in 0..master_stack.len() {
+        if i as i32 != 0 {
+            prev = prev ^ master_stack[i];
+        }
+    }
 
-  prev
+    // If, master bit is present in the stack, then set to stack size minus 1
+    // Else, set to stack size
+    let stack_len: usize = if bits[0] == 1 {
+        master_stack.len() - 1
+    } else {
+        master_stack.len()
+    };
+
+    // If, EVEN number of ones are present in stack (not including master bit, if present),
+    // And, master bit indicates EVEN number of ones, Then set to 0
+    // Else If, ODD number of ones are present in stack (not including master bit, if present),
+    // And, master bit indicates ODD number of ones, Then set to 0
+    // Else, set to 1
+    let master_fault: i32 = if stack_len % 2 == 0 && bits[0] == 0 {
+        0
+    } else if stack_len % 2 != 0 && bits[0] == 1 {
+        0
+    } else {
+        1
+    };
+
+    //* RETURNS:
+    //* [Location of errored bit (IS ONE OF 0..16), If master bit is wrong (IS EITHER 0 or 1)]
+    [prev, master_fault]
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,51 @@
 use data_validity_testing::data_checker;
+use colored::*;
+
+fn print_formatted_bits(bits: &[i32; 16]) {
+    println!("bits = [");
+    println!(
+        "  {a}, {b}, {c}, {d},",
+        a = bits[0],
+        b = bits[1],
+        c = bits[2],
+        d = bits[3]
+    );
+    println!(
+        "  {a}, {b}, {c}, {d},",
+        a = bits[4],
+        b = bits[5],
+        c = bits[6],
+        d = bits[7]
+    );
+    println!(
+        "  {a}, {b}, {c}, {d},",
+        a = bits[8],
+        b = bits[9],
+        c = bits[10],
+        d = bits[11]
+    );
+    println!(
+        "  {a}, {b}, {c}, {d}",
+        a = bits[12],
+        b = bits[13],
+        c = bits[14],
+        d = bits[15]
+    );
+    println!("]\n");
+}
+
+fn hamming_test() {
+    println!("\n\n{}", "HAMMING'S CODE".green().underline());
+    let bits: [i32; 16] = [0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1];
+    let hamming_results = data_checker::hamming(&bits);
+    print_formatted_bits(&bits);
+    println!(
+        "Position: {}\nMaster Fault: {}",
+        hamming_results[0],
+        hamming_results[1]
+    );
+}
 
 fn main() {
-    let bits: [i32; 16] = [
-        0, 0, 1, 1,
-        1, 0, 1, 0,
-        0, 0, 0, 0,
-        1, 0, 0, 1
-    ];
-    println!("{:?}", bits);
-    println!("{}", data_checker::hamming(bits));
+    hamming_test();
 }
